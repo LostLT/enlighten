c3c94a2977411dc94046d94e696b5b642d1850e0
Adam <adamdials@unity3d.com>
2015-10-09 15:34:20 -0500

    testing createPollingJobs

---
+++ Assets/guipuke.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class guipuke : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+    void OnGUI()
+    {
+        GUI.TextArea(new Rect(10,10,100,100), "This better not say nuthing about Marcus");
+
+    }
+}
